f5acf195c2344d6580c4e424a45ddbe80b6bd384
Marc Lang <tradecraft9@gmail.com>
2015-01-30 20:49:14 -0500

    procfile

--- dripfeed/config.js
+++ dripfeed/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   twitter: {
-    consumer_key        : 'put-yours-here',
-    consumer_secret     : 'put-yours-here',
-    access_token_key    : 'put-yours-here',
-    access_token_secret : 'put-yours-here'
+    consumer_key        : 'EBmqUcmDWHKV4nUkgGBS7iGAt',
+    consumer_secret     : 'MGaJSRABde4NlFVec1FMmjWIzFzPuLFN2DBwLwzo9xFbTq1ydz',
+    access_token_key    : '2992487241-V0mMWD89VZPFKuDDxsZbogPcoWCDf077J1dzfRL',
+    access_token_secret : 'wbzWmSQvQD0cAxQJsRDjKvS9mx8rSr1kKz64xfbhMYt8Y'
   }
 }
